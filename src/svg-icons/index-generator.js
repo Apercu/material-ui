ac1bdd9a978659cbd229ba1cdc8e0222d5e6785b
Balthazar Gronon <bgronon@gmail.com>
2015-12-17 00:55:07 +0000

    refactor(icons-generator): performance improvements and generate es6

--- src/svg-icons/index-generator.js
+++ src/svg-icons/index-generator.js
@@ -1,38 +1,31 @@
-import fs from 'fs';
-import rrs from 'recursive-readdir-sync';
+const fs = require('fs');
+const rrs = require('recursive-readdir-sync');
 
 const outArray = [];
-outArray.push('export default {\n');
 
-rrs('./').forEach(function(file) {
-	if(file !== 'index-generator.js' && file !== 'index.js')
-	{
-		var fileLines = fs.readFileSync(file, 'utf8').split('\n');
-		var index = 0, found = false;
+rrs('./').forEach(function (file) {
+  if (file !== 'index-generator.js' && file !== 'index.js') {
 
-		while(found === false && index < fileLines.length)
-		{
-			if(fileLines[index].indexOf('module.exports') > -1)
-			{
-				var moduleName = fileLines[index].split('=')[1].replace(';','').trim();
+    var fileLines = fs.readFileSync(file, 'utf8').split('\n');
+    var lineIndex = fileLines.length - 1;
 
-				outArray.push('\t');
-				outArray.push(moduleName);
-				outArray.push(': require(\'./');
-				outArray.push(file.substring(0, file.length - 4));
-				outArray.push('\'),\n');
+    while (lineIndex > 0) {
+      var line = fileLines[lineIndex];
+      var i = line.indexOf('export default')
 
-				found = true;
-			}
+      if (i !== -1) {
+        moduleName = line.substr(i + 15).trim().slice(0, -1);
+        outArray.push('export ');
+        outArray.push(moduleName);
+        outArray.push(' from \'./');
+        outArray.push(file.substring(0, file.length - 4));
+        outArray.push('\';\n');
+        break;
+      }
 
-			else
-			{
-				index++;
-			}
-		}
-	}
+      --lineIndex;
+    }
+  }
 });
-
-outArray.push('\n};\n')
 
 fs.writeFileSync('index.js', outArray.join(''));
